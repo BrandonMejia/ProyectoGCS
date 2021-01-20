8c0aa8a6de81b03bc248762edd42eaac60c62c0f
Martin Martinez Bravo <54995790+martiinbra@users.noreply.github.com>
2021-01-20 16:46:24 -0500

    Add files via upload

---
+++ Seccion Desarrollo/SVC19/Fuentes/base de datos/migraciones/2021_01_20_051112_centros.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Centros extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('centros', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->integer('capacidad');
+            $table->unsignedBiginteger('distrito_id');
+            $table->foreign('distrito_id') 
+                -> references ('id') -> on ('distritos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('centros');
+    }
+}
